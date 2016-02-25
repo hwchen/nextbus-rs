b9cd5d77d466225dc6bc2211da9d44ca50f2a463
Walther Chen <walther.chen@gmail.com>
2016-02-24 21:42:38 -0500

    route_config part 2
    - route config at least parses one route
    
    will have to check next if it correctly parses more than one route, and
    at least have some tests.

--- src/api/route_config.rs
+++ src/api/route_config.rs
@@ -145,7 +145,10 @@ pub struct Direction {
 
 /// The coordinates tracing a Route
 #[derive(Debug)]
-pub struct Path(Vec<Point>);
+pub struct Path {
+    tag: String,
+    points: Vec<Point>,
+}
 
 /// A coordinate in a Path
 #[derive(Debug)]
@@ -207,7 +210,6 @@ fn add_route_to_routes<R: Read>(mut parser: &mut EventReader<R>,
         lat_max: try!(lat_max.ok_or(Error::ParseError)),
         lon_min: try!(lon_min.ok_or(Error::ParseError)),
         lon_max: try!(lon_max.ok_or(Error::ParseError)),
-        // temporary
         stops: stops,
         directions: directions,
         paths: paths,
@@ -223,7 +225,7 @@ fn add_route_to_routes<R: Read>(mut parser: &mut EventReader<R>,
 // is no end element for stops. So stops won't know when to end until
 // it hits a direction start tag.
 // ===============================================================
-fn parse_route_elements<R: Read>(parser: &mut EventReader<R>,
+fn parse_route_elements<R: Read>(mut parser: &mut EventReader<R>,
                         mut stops: &mut Vec<Stop>,
                         mut directions: &mut Vec<Direction>,
                         mut paths: &mut Vec<Path>) -> ::Result<()> {
@@ -231,13 +233,32 @@ fn parse_route_elements<R: Read>(parser: &mut EventReader<R>,
     // - continue if it's a start element that's a stop
     // - break if it's a start element that's not a stop (and move on)
     // - break on error
+    // - break when hit end route
     // - continue if it's any other event (whitespace, etc.)
     loop {
         match parser.next() {
             Ok(XmlEvent::StartElement {name, attributes, ..}) => {
-                if name.borrow().local_name == "stop" {
+                let name = name.borrow().local_name;
+                if name == "stop" {
+                    // Stops are not nested inside a stop start/end,
+                    // so can directly add them to the vec here
                     try!(add_stop_to_stops(attributes, &mut stops));
-                } else {
+                } else if name == "direction" {
+                    // Direction contains a start/end tag, so the stops
+                    // in direction are nested, so can't add here directly.
+                    // Need to parse all Direction struct, like parsing a 
+                    // route.
+                    try!(parse_direction(&mut parser, attributes, &mut directions));
+                } else if name == "path" {
+                    // path element has no attributes
+                    // tag is contained in inner element
+                    try!(parse_path(&mut parser, &mut paths));
+                } else { // This shouldn't be needed.
+                    break;
+                }
+            },
+            Ok(XmlEvent::EndElement {name, ..}) => {
+                if name.borrow().local_name == "route" {
                     break;
                 }
             },
@@ -284,21 +305,49 @@ fn add_stop_to_stops(attributes: Vec<OwnedAttribute>,
     Ok(())
 }
 
-// Parsing Directions
+// Parsing Direction
 // ===============================================================
-fn parse_directions<R: Read>(parser: &mut EventReader<R>,
-                             mut stops: &mut Vec<Stop>) -> ::Result<()> {
+fn parse_direction<R: Read>(parser: &mut EventReader<R>,
+                             attributes: Vec<OwnedAttribute>,
+                             mut directions: &mut Vec<Direction>) -> ::Result<()> {
+    // put attributes of direction first
+    let mut tag = None;
+    let mut title = None;
+    let mut name = None;
+    let mut use_for_ui: Option<bool> = None;
+
+    for attribute in attributes {
+        let attribute = attribute.borrow();
+        let attr_name = attribute.name.local_name;
+        let value = attribute.value;
+
+        match attr_name {
+            "tag" => tag = Some(value.to_owned()),
+            "title" => title = Some(value.to_owned()),
+            "name" => name = Some(value.to_owned()),
+            "useForUI" => use_for_ui = Some(value.parse().unwrap()),
+            _ => (),
+        };
+    }
+
+    // Now iterate through inner Stop tags
     // The logic for matching:
     // - continue if it's a start element that's a stop
-    // - break if it's a start element that's not a stop (and move on)
+    // - break if it's an end element for direction (The next one should be matching)
     // - break on error
     // - continue if it's any other event (whitespace, etc.)
+    let mut stops = Vec::new();
     loop {
         match parser.next() {
             Ok(XmlEvent::StartElement {name, attributes, ..}) => {
+                if name.borrow().local_name == "stop" {
+                    //println!("{:?}", name);
+                    //println!("{:?}", attributes);
+                    try!(add_stop_to_direction(attributes, &mut stops));
+                }
+            },
+            Ok(XmlEvent::EndElement {name, ..}) => {
                 if name.borrow().local_name == "direction" {
-                    try!(add_stop_to_stops(attributes, &mut stops));
-                } else {
                     break;
                 }
             },
@@ -306,57 +355,111 @@ fn parse_directions<R: Read>(parser: &mut EventReader<R>,
             _ => continue,
         }
     }
+    directions.push(Direction {
+        tag: try!(tag.ok_or(Error::ParseError)),
+        title: try!(title.ok_or(Error::ParseError)),
+        name: try!(name.ok_or(Error::ParseError)),
+        use_for_ui: try!(use_for_ui.ok_or(Error::ParseError)),
+        stops: stops,
+    });
     Ok(())
 }
+
 fn add_stop_to_direction(attributes: Vec<OwnedAttribute>,
-                         stop_stubs: &mut Vec<StopStub>) -> ::Result<()> {
-                        // Now get directions for this route
-                        // The logic for matching:
-                        // - continue if it's a start element that's a stop
-                        // - break if it's a start element that's not a stop (and move on)
-                        // - break on error
-                        // - continue if it's any other event (whitespace, etc.)
-//                        let mut directions = Vec::new();
-//                        loop {
-//                            match parser.next() {
-//                                Ok(XmlEvent::StartElement {name, attributes, ..}) => {
-//                                    let name = name.borrow().local_name;
-//
-//                                    if name == "direction" {
-//                                        let tag = None;
-//                                        let title = None;
-//                                        let name = None;
-//                                        let userForUI: Option<bool> = None;
-//
-//                                        for attribute in attributes {
-//                                            let attribute = attribute.borrow();
-//                                            let name = attribute.name.local_name;
-//                                            let value = attribute.value;
-//
-//                                            match name {
-//                                                "tag" => tag = Some(value.to_owned()),
-//                                                "title" => title = Some(value.to_owned()),
-//                                                "color" => color = Some(value.to_owned()),
-//                                                "name" => name = Some(value.to_owned()),
-//                                                "useForUI" => use_for_ui = Some(value.parse().unwrap()),
-//                                                _ => (),
-//                                            };
-//                                        }
-//
-//
-//                                    } else if name == "stop" {
-//                                        try!(add_stop_to_direction(attributes, &mut stops));
-//                                    } else {
-//                                        break;
-//                                    }
-//                                },
-//                                Err(_) => break,
-//                                _ => continue,
-//                            }
-//                        }
-//                        // Finish direction
+                         stops: &mut Vec<StopStub>) -> ::Result<()> {
+    let mut tag = None;
+
+    for attribute in attributes {
+        let attribute = attribute.borrow();
+        let name = attribute.name.local_name;
+        let value = attribute.value;
+
+        match name {
+            "tag" => tag = Some(value.to_owned()),
+            _ => (),
+        };
+    }
+    stops.push(StopStub {
+        tag: try!(tag.ok_or(Error::ParseError)),
+    });
     Ok(())
 }
+
+// Parsing Path
+// ===============================================================
+fn parse_path<R: Read>(parser: &mut EventReader<R>,
+                       mut paths: &mut Vec<Path>) -> ::Result<()> {
+    // put attributes of direction first
+    let mut tag = None;
+
+
+    // Now iterate through inner 
+    // The logic for matching:
+    // - continue if it's a start element that's a tag or point
+    // - break if it's an end element for path (The next one should be matching)
+    // - break on error
+    // - continue if it's any other event (whitespace, etc.)
+    let mut points = Vec::new();
+    loop {
+        match parser.next() {
+            Ok(XmlEvent::StartElement {name, attributes, ..}) => {
+                let name = name.borrow().local_name;
+
+                if name == "tag" {
+                    for attribute in attributes {
+                        let attribute = attribute.borrow();
+                        let attr_name = attribute.name.local_name;
+                        let value = attribute.value;
+
+                        match attr_name {
+                            "id" => tag = Some(value.to_owned()),
+                            _ => (),
+                        };
+                    }
+
+                } else if name == "point" {
+                    try!(add_point_to_path(attributes, &mut points));
+                }
+            },
+            Ok(XmlEvent::EndElement {name, ..}) => {
+                if name.borrow().local_name == "path" {
+                    break;
+                }
+            },
+            Err(_) => break,
+            _ => continue,
+        }
+    }
+    paths.push(Path {
+        tag: try!(tag.ok_or(Error::ParseError)),
+        points: points,
+    });
+    Ok(())
+}
+
+fn add_point_to_path(attributes: Vec<OwnedAttribute>,
+                     points: &mut Vec<Point>) -> ::Result<()> {
+    let mut lat: Option<f32> = None;
+    let mut lon: Option<f32> = None;
+
+    for attribute in attributes {
+        let attribute = attribute.borrow();
+        let name = attribute.name.local_name;
+        let value = attribute.value;
+
+        match name {
+            "lat" => lat = Some(value.parse().unwrap()),
+            "lon" => lon = Some(value.parse().unwrap()),
+            _ => (),
+        };
+    }
+    points.push(Point {
+        lat: try!(lat.ok_or(Error::ParseError)),
+        lon: try!(lon.ok_or(Error::ParseError)),
+    });
+    Ok(())
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -366,7 +469,7 @@ mod test {
     fn should_get_one_route_config() {
         let routes = RouteConfigBuilder::new()
             .agency("mit")
-            .route("saferidecambwest")
+            .route("saferidecampshut")
             .get()
             .unwrap();
         for route in routes {
